bf50a18882b6921bfd800f1232af13ac12d3b7e3
Antônio Thiago Neto <atn.coding@gmail.com>
2024-05-27 23:50:43 -0300

    IGNORE

---
+++ epa-control/src/main/java/com/api/epacontrol/configs/DbConfig.java
@@ -0,0 +1,18 @@
+package com.api.epacontrol.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+  @Value("${DB_URL}")
+  private String dbUrl;
+
+  @Value("${DB_USERNAME}")
+  private String dbUsername;
+
+  @Value("${DB_PASSWORD}")
+  private String dbPassword;
+  // ... configure your data source using dbUrl, dbUsername, and dbPassword
+}
