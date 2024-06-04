e144a2f1352c6cc2d6870ded73318a78c4e0e3d0
Antônio Thiago Neto <atn.coding@gmail.com>
2024-06-04 17:20:37 -0300

    Revert "dbconfig"
    This reverts commit 91d114156905652f415fedd0b678ace72bf202e3.

--- epa-control/src/main/java/com/api/epacontrol/configs/DbConfig.java
+++ epa-control/src/main/java/com/api/epacontrol/configs/DbConfig.java
@@ -1,18 +1,18 @@
-// package com.api.epacontrol.configs;
+package com.api.epacontrol.configs;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// public class DbConfig {
+@Configuration
+public class DbConfig {
 
-//   @Value("${DB_URL}")
-//   private String dbUrl;
+  @Value("${DB_URL}")
+  private String dbUrl;
 
-//   @Value("${DB_USERNAME}")
-//   private String dbUsername;
+  @Value("${DB_USERNAME}")
+  private String dbUsername;
 
-//   @Value("${DB_PASSWORD}")
-//   private String dbPassword;
-//   // ... configure your data source using dbUrl, dbUsername, and dbPassword
-// }
+  @Value("${DB_PASSWORD}")
+  private String dbPassword;
+  // ... configure your data source using dbUrl, dbUsername, and dbPassword
+}
