6b76c2722dc973140db0207c947b0123570b148a
Antônio Thiago Neto <atn.coding@gmail.com>
2024-06-05 18:02:27 -0300

    create turma

--- epa-control/src/main/java/com/api/epacontrol/repositories/LocalizacaoTurmaRepository.java
+++ epa-control/src/main/java/com/api/epacontrol/repositories/LocalizacaoTurmaRepository.java
@@ -1,9 +1,16 @@
 package com.api.epacontrol.repositories;
 
 import com.api.epacontrol.models.LocalizacaoTurmaModel;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalizacaoTurmaRepository
-  extends JpaRepository<LocalizacaoTurmaModel, Long> {}
+  extends JpaRepository<LocalizacaoTurmaModel, UUID> {
+  List<LocalizacaoTurmaModel> findByEnderecoAndCidade(
+    String endereco,
+    String cidade
+  );
+}
