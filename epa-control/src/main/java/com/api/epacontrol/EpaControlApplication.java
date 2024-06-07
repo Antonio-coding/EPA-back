3ec723e951376d5e7cd5382dd9a61fd0ffa63f81
Antônio Thiago Neto <atn.coding@gmail.com>
2024-06-07 14:15:25 -0300

    environment variables

--- epa-control/src/main/java/com/api/epacontrol/EpaControlApplication.java
+++ epa-control/src/main/java/com/api/epacontrol/EpaControlApplication.java
@@ -1,18 +1,28 @@
 package com.api.epacontrol;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EpaControlApplication {
 
   public static void main(String[] args) {
+    // Load environment variables from .env file
+    Dotenv dotenv = Dotenv.configure().load();
+    dotenv
+      .entries()
+      .forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+    // Run the Spring Boot application
     SpringApplication.run(EpaControlApplication.class, args);
   }
 
   @GetMapping("/")
   public String index() {
-    return "Ola Mundo thiago";
+    return "Ola Mundo Thiago";
   }
 }
